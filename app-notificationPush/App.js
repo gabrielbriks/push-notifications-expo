241a740d2407887445510b324081906c64d45bf4
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-03-09 13:48:07 -0300

    Notification Push - Implementando funcionamento da notificação push

--- app-notificationPush/App.js
+++ app-notificationPush/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
+  useEffect(() => {
+    this.getPushNotificationPermissions();
+  }, []);
+
+  getPushNotificationPermissions = async () => {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+
+    let finalStatus = existingStatus;
+
+    /* Só perguntar se as permissões ainda não foram determinadas, porque
+    o iOS não necessariamente solicitará ao usuário uma segunda vez.
+    */
+    if (existingStatus !== "granted") {
+      // ?Concedito
+      /*
+       Permissões de notificação remota do Android são concedidas durante a instalação do aplicativo,
+       então isso só vai pedir no iOS.
+      */
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    // Pare aqui se o usuário não concedeu permissões
+    if (finalStatus !== "granted") {
+      return;
+    }
+    console.log(finalStatus);
+
+    // Obtenha o token que identifica este dispositivo exclusivamente
+    console.log(
+      "Notification Token: ",
+      await Notifications.getExpoPushTokenAsync()
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
